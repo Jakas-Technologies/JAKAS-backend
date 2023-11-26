f7a180de71d3a40d553ff529dc9e75f5e38c3fa2
gammarw <42285633+ziqu98@users.noreply.github.com>
2023-11-26 16:45:47 +0700

    First commit

---
+++ authServer.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const users = require('./users')
+const refreshTokens = require('./refreshTokens')
+
+app.use(express.json())
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshToken.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name: user.name })
+        res.json({ accessToken: accessToken })
+    })
+})
+
+app.post('/register', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user != null) {
+        return res.status(409).send('User already exists')
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send('User successfully created')
+    } catch {
+        res.status(500).send()
+    }
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+app.post('/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+        return res.status(400).send('User not found')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const accessToken = generateAccessToken(user)
+            const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+            refreshTokens.push(refreshToken)
+            res.json({ accessToken: accessToken, refreshToken: refreshToken }).send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
+function generateAccessToken(user)  {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
+
+app.listen(4000)
