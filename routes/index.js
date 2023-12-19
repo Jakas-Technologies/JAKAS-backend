0201ab63b75af3604f225870edc23d4070d5d712
gammarw <42285633+ziqu98@users.noreply.github.com>
2023-12-19 17:40:04 +0700

    Add fare routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const userRoute = require("./user/user");
 const driverRoute = require("./driver/driver");
 const trackRoute = require("./track/tracker");
-const FareController = require("./fare/model_deployment/controllers/FareController")
+const fareRoute = require("./fare/fare")
 
-const allRoutes = [userRoute, driverRoute, trackRoute, FareController];
+const allRoutes = [userRoute, driverRoute, trackRoute, fareRoute];
 
 const initRoutes = (app) => {
   allRoutes.forEach((router) => {
