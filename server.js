5bcb63300b833ac6c73aa942c73af5233f79bc49
gammarw <42285633+ziqu98@users.noreply.github.com>
2023-12-19 16:49:14 +0700

    Add socket.io auth handler

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-require('dotenv').config()
-
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const cors = require('cors')
 const http = require('http')
 const socketio = require('socket.io')
+const db = require("./models")
+const { initRoutes } = require("./routes");
 
-const app = express()
+let app = express()
 const port = 3000
-app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app = initRoutes(app)
 app.get('/',(req, res) => {
     res.status(200).send({
-        error: false,
+        success: true,
         message: 'Welcome to JAKAS'
     })
-})
-
-app.get('/users', authenticateToken, (req, res) => {
-    res.json(req.user)
 })
 
 const server = http.createServer(app)
@@ -36,17 +32,26 @@ io.on('connection', (socket) => {
     console.log(socket.id)
 })
 
-function authenticateToken(req, res, next)  {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
-    
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
-}
+io.use((socket, next) => {
+    if (socket.handshake.headers.auth) {
+      const { auth } = socket.handshake.headers;
+      const token = auth.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decodedToken) => {
+        if (err) {
+          throw new Error("Authentication error, Invalid Token supplied");
+        }
+        const theUser = await db.User.findByPk(decodedToken.id);
+        if (!theUser)
+          throw new Error(
+            "Invalid Email or Password, Kindly contact the admin if this is an anomaly"
+          );
+        socket.theUser = theUser;
+        return next();
+      });
+    } else {
+      throw new Error("Authentication error, Please provide a token");
+    }
+});
 
 server.listen(port, () => {
     console.log(`JAKAS app listening on port ${port}`)
