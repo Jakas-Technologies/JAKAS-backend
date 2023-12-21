ba5d77dcf4f592e09aff1476d0e11ca108b5113f
gammarw <42285633+ziqu98@users.noreply.github.com>
2023-12-21 22:55:24 +0700

    Update main route handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const userRoute = require("./user/user");
 const driverRoute = require("./driver/driver");
 const trackRoute = require("./track/tracker");
-const fareRoute = require("./fare/fare")
+const fareRoute = require("./fare/fare");
+const paymentRoute = require("./payment/payment")
 
-const allRoutes = [userRoute, driverRoute, trackRoute, fareRoute];
+const allRoutes = [userRoute, driverRoute, trackRoute, fareRoute, paymentRoute];
 
 const initRoutes = (app) => {
   allRoutes.forEach((router) => {
