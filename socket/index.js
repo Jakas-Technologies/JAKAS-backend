09541aee3665e29a1294b3d577624bef868f37a5
gammarw <42285633+ziqu98@users.noreply.github.com>
2023-12-21 22:26:17 +0700

    Update socket files

--- socket/index.js
+++ socket/index.js
@@ -1,14 +1,22 @@
 const { hoistedIODriver } = require("./driversocket");
 const { hoistedIOUser } = require("./usersocket");
+const db = require("../models")
+
 const configureSockets = (io, socket) => {
   return {
     driverLocation: hoistedIODriver(io),
     userLocation: hoistedIOUser(io),
   };
 };
-const onConnection = (io) => (socket) => {
+
+const onConnection = (io) => async (socket) => {
+  console.log(socket)
+  const geo = await db.Geolocation.update(
+    { socketID: socket.id },
+    { where: { id: socket.user.id } }
+  )
   const { userLocation, driverLocation } = configureSockets(io, socket);
-  socket.on("user-move", userLocation);
-  socket.on("driver-move", driverLocation);
+  socket.on("usermove", userLocation);
+  socket.on("drivermove", driverLocation);
 };
 module.exports = { onConnection };
